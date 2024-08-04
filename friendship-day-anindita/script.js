7bc6bba66a03384803127577eec37ba42cc02dfc
Dhritiman Saikia <147172527+dhritiman78@users.noreply.github.com>
2024-08-04 12:59:08 +0530

    Create script.js

---
+++ friendship-day-anindita/script.js
@@ -0,0 +1,13 @@
+// script.js
+document.getElementById('happyButton').addEventListener('click', function() {
+    window.location.href = "upi://pay?pa=arunimask100-1@oksbi&pn=Anindita%20Saikia&am=1&cu=INR&aid=uGICAgMDEuefQcA"
+});
+
+// Change background color on hover over the container
+document.querySelector('.container').addEventListener('mouseover', function() {
+    document.body.style.backgroundColor = "#e6e6fa";
+});
+
+document.querySelector('.container').addEventListener('mouseout', function() {
+    document.body.style.backgroundColor = "#f0f8ff";
+});
